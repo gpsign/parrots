37fdd9eef37e713303eabdd801fa324974d1ee97
gpsign <gpsignorini@gmail.com>
2023-09-02 13:17:02 -0300

    change: changed to a better README change: now checks for a "y" or "n" at the restart alert

--- JS/script.js
+++ JS/script.js
@@ -7,43 +7,44 @@ let funcaoTempo;
 let tela;
 let deck = [];
 let buffer = [];
-let pares = [
-  "bobrossparrot",
-  "explodyparrot",
-  "fiestaparrot",
-  "metalparrot",
-  "revertitparrot",
-  "tripletsparrot",
-  "unicornparrot",
+const pares = [
+	"bobrossparrot",
+	"explodyparrot",
+	"fiestaparrot",
+	"metalparrot",
+	"revertitparrot",
+	"tripletsparrot",
+	"unicornparrot",
 ];
 
 setInterval(ajustaTela, 100);
 comecarJogo();
 
 function comecarJogo() {
-  pares.sort(comparador);
-  deck = [];
-  nmrCartas = 0;
-  jogadas = 0;
-  paresFeitos = 0;
-  tempo = 0;
+	pares.sort(comparador);
+	deck = [];
+	nmrCartas = 0;
+	jogadas = 0;
+	paresFeitos = 0;
+	tempo = 0;
 
-  while (nmrCartas < 4 || nmrCartas > 14 || nmrCartas % 2 !== 0) {
-    nmrCartas = prompt("Numero de cartas:");
-  }
+	while (nmrCartas < 4 || nmrCartas > 14 || nmrCartas % 2 !== 0) {
+		nmrCartas = prompt("Numero de cartas:");
+	}
 
-  gerarDeck();
+	gerarDeck();
 
-  mesa.innerHTML = deck.join(" ");
+	mesa.innerHTML = deck.join(" ");
 
-  document.querySelector(".cronometro p").innerHTML = 0;
-  funcaoTempo = setInterval(cronometrar, 1000);
+	document.querySelector(".cronometro p").innerHTML = 0;
+	funcaoTempo = setInterval(cronometrar, 1000);
 }
 
 function gerarDeck() {
-  for (let i = 0; i < nmrCartas / 2; i++) {
-    for (let j = 0; j < 2; j++) {
-      deck.push(`<div class="card" data-test="card" onclick="clicou(this)">
+	for (let i = 0; i < nmrCartas / 2; i++) {
+		for (let j = 0; j < 2; j++) {
+			deck.push(`
+      <div class="card" data-test="card" onclick="clicou(this)">
         <div class="front-face face">
           <img data-test="face-down-image" src="./imgs/back.png"></img>
         </div>
@@ -51,97 +52,97 @@ function gerarDeck() {
           <img data-test="face-up-image" src="./imgs/${pares[i]}.gif"></img>
         </div>
       </div>`);
-    }
-    deck.sort(comparador);
-  }
+		}
+		deck.sort(comparador);
+	}
 }
 
 function cronometrar() {
-  tempo++;
-  document.querySelector(".cronometro p").innerHTML = tempo;
+	tempo++;
+	document.querySelector(".cronometro p").innerHTML = tempo;
 }
 
 function ajustaTela() {
-  tela = window.matchMedia("(max-width: 1024px)");
-  if (!tela.matches) {
-    mesa.style.gridTemplateColumns = `repeat(${deck.length / 2}, 117px)`;
-    mesa.style.gridTemplateRows = `repeat(2, 146px)`;
-  } else {
-    mesa.style.gridTemplateColumns = "117px";
-    mesa.style.gridTemplateRows = `repeat(${deck.length}, 146px)`;
-  }
+	tela = window.matchMedia("(max-width: 1024px)");
+	if (!tela.matches) {
+		mesa.style.gridTemplateColumns = `repeat(${deck.length / 2}, 117px)`;
+		mesa.style.gridTemplateRows = `repeat(2, 146px)`;
+	} else {
+		mesa.style.gridTemplateColumns = "117px";
+		mesa.style.gridTemplateRows = `repeat(${deck.length}, 146px)`;
+	}
 }
 
 function comparador() {
-  return Math.random() - 0.5;
+	return Math.random() - 0.5;
 }
 
 function clicou(carta) {
-  if (buffer.length < 2 && !carta.classList.contains("virada")) {
-    buffer.push(carta);
-    jogadas++;
-    virar();
-    verificarJogada();
-  }
+	if (buffer.length < 2 && !carta.classList.contains("virada")) {
+		buffer.push(carta);
+		jogadas++;
+		virar();
+		verificarJogada();
+	}
 }
 
 function virar() {
-  buffer[buffer.length - 1].firstElementChild.style.transform =
-    "rotateY(-180deg)";
-  buffer[buffer.length - 1].lastElementChild.style.transform = "rotateY(0deg)";
-  buffer[buffer.length - 1].classList.add("virada");
+	buffer[buffer.length - 1].firstElementChild.style.transform =
+		"rotateY(-180deg)";
+	buffer[buffer.length - 1].lastElementChild.style.transform = "rotateY(0deg)";
+	buffer[buffer.length - 1].classList.add("virada");
 }
 
 function verificarJogada() {
-  if (!verificarCarta()) setTimeout(desvirar, 1000);
-  else if (paresFeitos === nmrCartas / 2) {
-    setTimeout(() => {
-      anunciarVitoria();
-      novoJogo();
-    }, 1000);
-  }
+	if (!verificarCarta()) setTimeout(desvirar, 1000);
+	else if (paresFeitos === nmrCartas / 2) {
+		setTimeout(() => {
+			anunciarVitoria();
+			novoJogo();
+		}, 500);
+	}
 }
 
 function verificarCarta() {
-  if (buffer.length === 2) {
-    if (
-      buffer[0].lastElementChild.firstElementChild.src ==
-      buffer[1].lastElementChild.firstElementChild.src
-    ) {
-      buffer = [];
-      paresFeitos++;
-      return true;
-    } else {
-      return false;
-    }
-  } else return true;
+	if (buffer.length === 2) {
+		if (
+			buffer[0].lastElementChild.firstElementChild.src ==
+			buffer[1].lastElementChild.firstElementChild.src
+		) {
+			buffer = [];
+			paresFeitos++;
+			return true;
+		} else {
+			return false;
+		}
+	} else return true;
 }
 
 function anunciarVitoria() {
-  clearInterval(funcaoTempo);
-  alert(
-    `Você ganhou em ${jogadas} jogadas! A duração do jogo foi de ${tempo} segundos!`
-  );
+	clearInterval(funcaoTempo);
+	alert(
+		`Você ganhou em ${jogadas} jogadas! A duração do jogo foi de ${tempo} segundos!`
+	);
 }
 
 function novoJogo() {
-  let reiniciar = "";
-  while (reiniciar !== "não" && reiniciar !== "sim") {
-    reiniciar = prompt("Começar um novo jogo?");
-    if (reiniciar === "sim") comecarJogo();
-  }
+	let reiniciar = "";
+	while (reiniciar !== "n" && reiniciar !== "s") {
+		reiniciar = prompt("Começar um novo jogo?");
+		if (reiniciar === "s") comecarJogo();
+	}
 }
 
 function desvirar() {
-  buffer[0].firstElementChild.style.transform = "rotateY(0deg)";
-  buffer[0].lastElementChild.style.transform = "rotateY(-180deg)";
+	buffer[0].firstElementChild.style.transform = "rotateY(0deg)";
+	buffer[0].lastElementChild.style.transform = "rotateY(-180deg)";
 
-  buffer[1].firstElementChild.style.transform = "rotateY(0deg)";
-  buffer[1].lastElementChild.style.transform = "rotateY(-180deg)";
+	buffer[1].firstElementChild.style.transform = "rotateY(0deg)";
+	buffer[1].lastElementChild.style.transform = "rotateY(-180deg)";
 
-  setTimeout(() => {
-    buffer[0].classList.remove("virada");
-    buffer[1].classList.remove("virada");
-    buffer = [];
-  }, 500);
+	setTimeout(() => {
+		buffer[0].classList.remove("virada");
+		buffer[1].classList.remove("virada");
+		buffer = [];
+	}, 500);
 }
--- README.md
+++ README.md
@@ -1,9 +1,27 @@
-# parrots
+# Parrots
 
-## Sobre
-Um simples jogo da memória utilizando HTML, CSS e JavaScript.
+A simple memory game using HTML, CSS and JavaScript
 
-## Ferramentas Utilizadas:
-- HTML
-- CSS
-- JavaScript
+<img src="./assets/parrots-usage.gif">
+
+Try it now at https://gpsign.github.io/parrots/
+
+## About
+
+This is an web application to play the classic memory game, this time themed around parrots! Below are the implemented features:
+
+- Choose an even number from 4 to 14 cards
+- In-game timer
+- How much cards you turned at the end
+- Start a new game by typing "y" at the end. Type "n" to stay at the end screen
+
+## Technologies
+
+The following tools were used in the construction of the project:<br>
+
+<p>
+<img style='margin: 5px;' src='https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white'>
+<img style='margin: 5px;' src='https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white'>
+<img style='margin: 5px;' src='https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E'>
+
+</p>
